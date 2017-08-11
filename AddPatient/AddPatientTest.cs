445e916d1cbc68f5824b39c659221373d8ff3906
nasv <k.nasvetova@ya.ru>
2017-08-11 22:54:47 +0300

    assert thet -> exception

--- AddPatient/AddPatientTest.cs
+++ AddPatient/AddPatientTest.cs
@@ -61,9 +61,25 @@ namespace AddPatient
             testPatient.BirthDate = new DateTime(1000, 05, 05);
             testPatient.IdPatientMIS = "1234567";
             testPatient.Sex = 1;
-            client.AddPatient(guid, idLPU, testPatient);
-            //необработанный эксепшн
-            Assert.Fail("Пациент был добавлен с неверной датой рождения.");
+            Assert.That(() => client.AddPatient(guid, idLPU, testPatient), Throws.Exception);
+            client.Close();
+        }
+
+        [Test]
+        public void AddPatient_MissParametr_IdPatientMIS()
+        {
+            string guid = "0310C7A6-BDF3-4124-B9D4-5FD5C72FA066";
+            string idLPU = "1.2.643.5.1.13.3.25.78.118";
+
+            PixService.PixServiceClient client = new PixService.PixServiceClient("BasicHttpBinding_IPixService");
+            PixService.PatientDto testPatient = new PixService.PatientDto();
+            testPatient.FamilyName = "Иванов";
+            testPatient.GivenName = "Petr";
+            testPatient.MiddleName = "Fedorovich";
+            testPatient.BirthDate = new DateTime(1993, 05, 05);
+            testPatient.Sex = 1;
+      
+            Assert.That(() => client.AddPatient(guid, idLPU, testPatient), Throws.Exception);
             client.Close();
         }
     }
